7c17d238ada1f579d1d3ce0e4f63a1a1d97ccb58
Usama0121 <aslam0121@gmail.com>
2022-12-20 14:45:17 +0500

    Fixed stix2 observable not being added due to missing modified key

--- opentaxii/persistence/sqldb/api.py
+++ opentaxii/persistence/sqldb/api.py
@@ -983,9 +983,26 @@ class Taxii2SQLDatabaseAPI(BaseSQLDatabaseAPI, OpenTAXII2PersistenceAPI):
         self.db.session.commit()
         job_details = []
         for obj in objects:
-            version = datetime.datetime.strptime(
-                obj["modified"], DATETIMEFORMAT
-            ).replace(tzinfo=datetime.timezone.utc)
+            if "modified" in obj:
+                version = datetime.datetime.strptime(
+                    obj["modified"], DATETIMEFORMAT
+                ).replace(tzinfo=datetime.timezone.utc)
+            elif "created" in obj:
+                version = datetime.datetime.strptime(
+                    obj["created"], DATETIMEFORMAT
+                ).replace(tzinfo=datetime.timezone.utc)
+            else:
+                # If a STIX object is not versioned (and therefore does not
+                # have a modified
+                # timestamp) then this version parameter MUST use the created
+                # timestamp. If
+                # an object does not have a created or modified timestamp or
+                # any other
+                # version information that can be used, then the server
+                # should use a value for
+                # the version that is consistent to the server.
+                # -- TAXII 2.1 specification --
+                version = obj["id"]
             if (
                 not self.db.session.query(literal(True))
                 .filter(
